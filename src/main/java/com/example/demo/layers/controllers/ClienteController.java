3b93932943b9190493c3faf45d980eff52120c04
Samuel Silva de Lima <samuclient@gmail.com>
2024-12-25 15:57:41 -0300

    feat: controler e service do cliente feitos

--- src/main/java/com/example/demo/layers/controllers/ClienteController.java
+++ src/main/java/com/example/demo/layers/controllers/ClienteController.java
@@ -1,9 +1,18 @@
 package com.example.demo.layers.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exceptions.ValidacaoException;
+import com.example.demo.layers.entities.Cliente;
 import com.example.demo.layers.services.ClienteService;
 
 @RestController
@@ -12,5 +21,33 @@ public class ClienteController {
     
     @Autowired
     ClienteService clienteService;
+    @PostMapping
+    public ResponseEntity<?> cadastrarCliente(@RequestBody Cliente cliente) {
+        try {
+            clienteService.cadastrarCliente(cliente);
+            return ResponseEntity.status(HttpStatus.OK).body("{}");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 
+    @PutMapping
+    public ResponseEntity<?> atualizarCliente(@RequestBody Cliente cliente) {
+        try {
+            clienteService.atualizarCliente(cliente);
+            return ResponseEntity.status(HttpStatus.OK).body("{}");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{idCliente}")
+    public ResponseEntity<?> removerCliente(@PathVariable Long idCliente) {
+        try {
+            clienteService.removerCliente(idCliente);
+            return ResponseEntity.status(HttpStatus.OK).body("{}");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/demo/layers/services/ClienteService.java
+++ src/main/java/com/example/demo/layers/services/ClienteService.java
@@ -1,8 +1,73 @@
 package com.example.demo.layers.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.exceptions.ValidacaoException;
+import com.example.demo.layers.entities.Cliente;
+import com.example.demo.layers.repositories.ClienteRepository;
+
+//FAZER OS PEDIDOS (NÃO SEIIIIII)
 
 @Service
 public class ClienteService {
-    
+     @Autowired
+    ClienteRepository clienteRepository;
+
+    public Cliente cadastrarCliente(Cliente cliente) throws ValidacaoException {
+
+        if(cliente.getId() != null) {
+            throw new ValidacaoException("ID nao nulo");
+        }
+
+        if(cliente.getEmail() == null) {
+            throw new ValidacaoException("Email inválido");
+        }
+
+        if(cliente.getNome() == null || cliente.getNome().isBlank()) {
+            throw new ValidacaoException("Nome inválido");
+        }
+
+        
+
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente atualizarCliente(Cliente cliente) throws ValidacaoException {
+
+        if(cliente.getId() == null) {
+            throw new ValidacaoException("ID é nulo");
+        }
+
+        Optional<Cliente> _cliente = clienteRepository.findById(cliente.getId());
+        if(_cliente.isEmpty()) {
+            throw new ValidacaoException("ID não existe");
+        }
+
+        if(cliente.getEmail() == null){
+            throw new ValidacaoException("Email inválido");
+        }
+
+        if(cliente.getNome() == null || cliente.getNome().isBlank()) {
+            throw new ValidacaoException("Nome inválido");
+        }
+
+        return clienteRepository.save(cliente);
+    }
+
+    public void removerCliente(Long idCliente) throws ValidacaoException  {
+        if(idCliente == null) {
+            throw new ValidacaoException("ID é nulo");
+        }
+
+        Optional<Cliente> _cliente = clienteRepository.findById(idCliente);
+        if(_cliente.isEmpty()) {
+            throw new ValidacaoException("ID não existe");
+        }
+        
+        clienteRepository.deleteById(idCliente);        
+    }
+
 }
